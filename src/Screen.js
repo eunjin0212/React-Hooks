f5a35994f200c24149669ccbbf6cafc058e88a87
eunjin0212 <eunjin9639@gmail.com>
2020-11-20 19:42:52 +0900

    UseContext

---
+++ src/Screen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+const Screen = () => {
+  return (
+    <div>
+      <Header />
+      <h1>First screen</h1>
+    </div>
+  );
+};
+
+export default Screen;
