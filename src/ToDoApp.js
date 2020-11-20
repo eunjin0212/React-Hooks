0a3074e6a5c099657fa4a1eb3faf49b39bd9abde
eunjin0212 <eunjin9639@gmail.com>
2020-11-20 22:26:17 +0900

    Understanding useReducer

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -3,13 +3,16 @@ const INCREMENT = "increment";
 const DECREMENT = "decrement";
 const reducer = (state, action) => {
   //state : 현재
-  if (action === INCREMENT) {
-    return { count: state.count + 1 };
-  } else if (action === DECREMENT) {
-    return { count: state.count - 1 };
+  switch (action) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
   }
+  //return 하는 object는 state를 "대체" 함, 변경 X
 };
-//return 하는 object는 state를 "대체" 함, 변경 X
 function App() {
   const [state, dispatch] = useReducer(reducer, { count: 0 });
   // `const reducer = (state = { count: 0 }, action) => {}`
@@ -19,7 +22,7 @@ function App() {
     <>
       <h1>{state.count}</h1>
       <button onClick={() => dispatch(INCREMENT)}>Add</button>
-      <button onClick={() => dispatch(DECREMENT)}>Minus</button>
+      <button onClick={() => dispatch(DECREMENT)}>Remove</button>
     </>
   );
 }
