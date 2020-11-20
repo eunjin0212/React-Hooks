52b811d1d325b1341768b34ca6982f12c25deb32
eunjin0212 <eunjin9639@gmail.com>
2020-11-20 23:18:42 +0900

    Adding To Dos

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -1,28 +1,44 @@
-import React, { useReducer } from "react";
-const INCREMENT = "increment";
-const DECREMENT = "decrement";
+import React, { useReducer, useState } from "react";
+
+const initialState = {
+  toDos: [],
+};
+const ADD = "increment";
+
 const reducer = (state, action) => {
-  //state : 현재
   switch (action) {
-    case INCREMENT:
+    case ADD:
       return { count: state.count + 1 };
-    case DECREMENT:
-      return { count: state.count - 1 };
+
     default:
-      throw new Error();
+      throw new Error("This is Error");
   }
-  //return 하는 object는 state를 "대체" 함, 변경 X
 };
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
-  // `const reducer = (state = { count: 0 }, action) => {}`
-  // = `reducer, { conut: 0 }` conut 부분이 state
-  // dispatch : action을 가지고 reducer function 다시 실행함
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewToDo] = useState("");
+  const onSubmit = (e) => {
+    e.preventDeafult();
+    dispatch({ type: ADD, payload: newToDo });
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewToDo(value);
+  };
+
   return (
     <>
-      <h1>{state.count}</h1>
-      <button onClick={() => dispatch(INCREMENT)}>Add</button>
-      <button onClick={() => dispatch(DECREMENT)}>Remove</button>
+      <h1>Add To Do</h1>
+      <form>
+        <input
+          value={newToDo}
+          type="text"
+          placeholder="Write to do"
+          onChange={onChange}
+        />
+      </form>
     </>
   );
 }
